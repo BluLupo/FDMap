88d277f6bd7e659ff1464ef4af3f7fae7136e213
LoricTai <lorictai@gmail.com>
2019-03-21 21:16:14 +0100

    arraycollection di sta gran cippa

--- src/Entity/FDUser.php
+++ src/Entity/FDUser.php
@@ -111,8 +111,8 @@ class FDUser implements UserInterface, \Serializable
 
     public function __construct()
     {
-        $this->sourceLogs = new \ArrayCollection();
-        $this->targetLogs = new \ArrayCollection();
+        $this->sourceLogs = new ArrayCollection();
+        $this->targetLogs = new ArrayCollection();
     }
 
 	public function getSalt()
